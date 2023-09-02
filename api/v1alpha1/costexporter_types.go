996b0a40c5b2ac8b128d3607c08b2c639026904a
Paul Shu <shuziteng1998@gmail.com>
2023-09-01 21:52:51 -0400

    Update costexporter_types.go

--- api/v1alpha1/costexporter_types.go
+++ api/v1alpha1/costexporter_types.go
@@ -1,52 +1,46 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // CostExporterSpec defines the desired state of CostExporter
 type CostExporterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of CostExporter. Edit costexporter_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// S3Bucket defines the AWS S3 bucket name where stores the cost logs.
+	S3Bucket string `json:"s3Bucket,omitempty"`
+	// CloudServiceProvider defines the target cloud service provide for calculating cost.
+	CloudServiceProvider string `json:"cloudServiceProvider,omitempty"`
+	// SecretRef defines the reference to the Kubernetes Secret where stores the credentials of cloud service provider
+	SecretRef corev1.ObjectReference `json:"secretRef,omitempty"`
 }
 
 // CostExporterStatus defines the observed state of CostExporter
 type CostExporterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// JobCompletionTime defines the completion time of the cost calculation job.
+	JobCompletionTime *metav1.Time `json:"jobCompletionTime,omitempty"`
+	// PodName defines the name of the cost calculation pod.
+	PodName string `json:"podName,omitempty"`
+	// JobStatus defines the status of the cost calculation job.
+	JobStatus string `json:"jobStatus,omitempty"`
+	// Tags defines the json string of using tags.
+	Tags string `json:"tags,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="JobCompletionTime",type="string",JSONPath=".status.jobCompletionTime"
+// +kubebuilder:printcolumn:name="JobStatus",type="string",JSONPath=".status.jobStatus"
+// +kubebuilder:printcolumn:name="PodName",type="string",JSONPath=".status.podName"
 
 // CostExporter is the Schema for the costexporters API
 type CostExporter struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   CostExporterSpec   `json:"spec,omitempty"`
+	// Spec defines the specifictions of the CostExporter.
+	Spec CostExporterSpec `json:"spec,omitempty"`
+	// Status defines the status of the CostExporter.
 	Status CostExporterStatus `json:"status,omitempty"`
 }
 
@@ -56,7 +50,8 @@ type CostExporter struct {
 type CostExporterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []CostExporter `json:"items"`
+	// Items defines a list of CostExporters.
+	Items []CostExporter `json:"items"`
 }
 
 func init() {
