3f11481224889a327d841c4fcb81c3d392518aa1
Baljit Singh <baljit92@gmail.com>
2023-09-02 04:09:55 +0000

    updated plantd core types

--- api/v1alpha1/plantdcore_types.go
+++ api/v1alpha1/plantdcore_types.go
@@ -27,9 +27,8 @@ type PrometheusSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of PlantDCore. Edit plantdcore_types.go to remove/update
-	MemorySize     int32   `json:"memorySize,omitempty"`
-	ScrapeInterval float32 `json:"scrapeInterval,omitempty`
+	// MemorySize     int `json:",omitempty"`
+	// monitoringv1.Scra `json:",omitempty"`
 }
 
 // PlantDCoreSpec defines the desired state of PlantDCore
@@ -38,7 +37,7 @@ type PlantDCoreSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of PlantDCore. Edit plantdcore_types.go to remove/update
-	PrometheusConfiguration PrometheusSpec `json:"prometheusConfiguration,omitempty`
+	PrometheusConfiguration PrometheusSpec `json:"prometheusConfiguration,omitempty"`
 }
 
 // PlantDCoreStatus defines the observed state of PlantDCore
