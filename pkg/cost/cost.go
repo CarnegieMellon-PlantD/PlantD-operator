a3f97f0fd9837920e2bb60930885858148727b3b
Liyang Zhu <tom20000201@gmail.com>
2024-02-16 14:02:26 -0500

    fix: get redis port from config

--- pkg/cost/cost.go
+++ pkg/cost/cost.go
@@ -41,11 +41,11 @@ func CreateJobByCostServie(ctx context.Context, jobName string, costService *win
 					Env: []corev1.EnvVar{
 						{
 							Name:  "EXPERIMENT_TAGS",
-							Value: string(costService.Status.Tags),
+							Value: costService.Status.Tags,
 						},
 						{
 							Name:  "S3_BUCKET_NAME",
-							Value: string(costService.Spec.S3Bucket),
+							Value: costService.Spec.S3Bucket,
 						},
 						{
 							Name:  "REDIS_HOST",
@@ -53,11 +53,11 @@ func CreateJobByCostServie(ctx context.Context, jobName string, costService *win
 						},
 						{
 							Name:  "REDIS_PORT",
-							Value: strconv.FormatInt(int64(6379), 10),
+							Value: strconv.FormatInt(config.GetInt64("database.redis.port"), 10),
 						},
 						{
 							Name:  "CLOUD_SERVICE_PROVIDER",
-							Value: string(costService.Spec.CloudServiceProvider),
+							Value: costService.Spec.CloudServiceProvider,
 						},
 						{
 							Name:  "EARLIEST_EXPERIMENT",
