354b87cd59805771477bba31321083721b91a18a
Baljit Singh <baljit92@gmail.com>
2024-06-01 23:02:32 +0200

    fix namespace rbac at global level

--- internal/controller/plantdcore_controller.go
+++ internal/controller/plantdcore_controller.go
@@ -35,6 +35,7 @@ type PlantDCoreReconciler struct {
 //+kubebuilder:rbac:groups=windtunnel.plantd.org,resources=plantdcores/finalizers,verbs=update
 //
 //+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=monitoring.coreos.com,resources=prometheuses,verbs=get;list;watch;create;update;patch;delete
